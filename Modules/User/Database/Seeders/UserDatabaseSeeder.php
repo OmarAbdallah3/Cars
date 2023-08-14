213138eda7631a2d4f9c5cb986d8b1bad519a2f2
Omar Abdallah <omarabdallahhk@gmail.com>
2023-08-14 20:10:32 +0200

    =seed commit

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Modules\User\Database\Seeders;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,6 +16,16 @@ class UserDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+
+        // Admin::create([
+        //     'first_name' => 'Omar ',
+        //     'middle_name'=> 'Abdallah',
+        //     'last_name' => 'Abdelmawgoud',
+        //     'email' => 'admin1@admin.com',
+        //     'password' => bcrypt(123456789),
+        //     'is_admin'=>'true',
+        //     'token'=>Str::uuid(Str::random(10)."admin1@admin1.com"),
+        // ]);
 
         // $this->call("OthersTableSeeder");
     }
